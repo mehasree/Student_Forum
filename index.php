af458d91141a90b92b8a9ddd39f3a38afb8d366d
RAJALAKSHMI.S <rajalakshmi.sarangan20@gmail.com>
2023-03-26 21:12:51 +0530

    login-php

--- index.php
+++ index.php
@@ -12,21 +12,29 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 else{
-    $select='SELECT userid From login_db Where userid= ? and pass= ? Limit 1 ';
-    $insert='INSERT Into login_success(userid) values(?)';
+    $select='SELECT userid From student_db Where userid= ? and pass= ? Limit 1 ';
+    $select1='SELECT userid From staff_db Where userid= ? and pass= ? Limit 1 ';
+    $insert='INSERT Into login_db(userid) values(?)';
     $stmt=$conn->prepare($select);
     $stmt->bind_param("ss",$userid,$pass);
     $stmt->execute();
     $stmt->bind_result($userid);
     $stmt->store_result();
     $rnum=$stmt->num_rows;
-    if($rnum==0)
+    $stmt1=$conn->prepare($select1);
+    $stmt1->bind_param("ss",$userid,$pass);
+    $stmt1->execute();
+    $stmt1->bind_result($userid);
+    $stmt1->store_result();
+    $rnum1=$stmt1->num_rows;
+    if($rnum==0 && $rnum1==0)
     {
         echo "Account doesn't exist";
         header("location: index.html");
     }
     else{
         $stmt->close();
+        $stmt1->close();
         $stmt=$conn->prepare($insert);
         $stmt->bind_param("s",$userid);
         $stmt->execute();
